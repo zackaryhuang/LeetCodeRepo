06d1ba29eecb1325c44df0cdc8d51afabb48072e
cageyoko <35170030+cageyoko@users.noreply.github.com>
2019-04-05 15:17:35 +0800

    Create 88. 合并两个有序数组.py

---
+++ Kaiq/88. 合并两个有序数组.py
@@ -0,0 +1,16 @@
+ """
+ 
+ """
+ 
+ 
+ class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:]=nums2
+        return  nums1.sort()
