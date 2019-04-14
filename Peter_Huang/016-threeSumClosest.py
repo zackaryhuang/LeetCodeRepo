4c4f79c59f3c43fcd212f51a8a2ff22626a5bacd
HuangChao <huangchaojszx@163.com>
2019-04-14 17:26:26 +0800

    Created 017-letterCombinations.py

--- Peter_Huang/016-threeSumClosest.py
+++ Peter_Huang/016-threeSumClosest.py
@@ -7,14 +7,12 @@ class Solution(object):
         :rtype: int
         """
         nums.sort()
-        print(nums)
         closestThreeSum = nums[0] + nums[1] + nums[2]
         for i in range(0,len(nums)-2):
             left = i + 1
             right = len(nums) - 1
             while left < right:
                 temp = nums[i] + nums[left] + nums[right]
-                print("temp:",temp)
                 if abs(target - temp) < abs(target - closestThreeSum):
                     closestThreeSum = temp
                 if temp > target:
@@ -22,9 +20,7 @@ class Solution(object):
                 elif temp < target:
                     left += 1
                 else:
-                    print(target)
                     return target
-        print('closestThreeSum:',closestThreeSum)
         return closestThreeSum
                 
 def main():
--- Peter_Huang/017-letterCombinations.py
+++ Peter_Huang/017-letterCombinations.py
@@ -1,1 +1,30 @@
-00
+from functools import reduce
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if digits == '':
+            return ""
+        l = []
+        d = []
+        for char in digits:
+            d.append(char)
+        dict = {'2':['a','b','c'],
+                '3':['d','e','f'],
+                '4':['g','h','i'],
+                '5':['j','k','l'],
+                '6':['m','n','o'],
+                '7':['p','q','r','s'],
+                '8':['t','u','v'],
+                '9':['w','x','y','z']}
+        for i in range(len(d)):
+            l.append(dict[d[i]])
+        fn = lambda x, code='': reduce(lambda x, y: [str(i)+code+str(j) for i in x for j in y], x)
+        res = fn(l,'')
+        return res
+if __name__ == "__main__":
+    s = Solution()
+    res = s.letterCombinations('')
+    print(res)
