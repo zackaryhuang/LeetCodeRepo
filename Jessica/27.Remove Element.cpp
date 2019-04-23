f3f2918f4dcb282749643e5d8fd3bb6cf97c2df2
Jessica <jiaohongyan>
2019-04-23 16:16:43 +0800

    fix

---
+++ Jessica/27.Remove Element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int>::iterator it;
+        for(it=nums.begin();it!=nums.end(); ){
+            if((*it)==val)               
+                nums.erase(it);
+            else
+                it++;            
+        }
+        
+        return nums.size();
+        
+    }
+};
---
+++ Jessica/27.Remove Element.py
@@ -0,0 +1,6 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        while val in nums:       
+            nums.remove(val)
+        return len(nums)
+        
