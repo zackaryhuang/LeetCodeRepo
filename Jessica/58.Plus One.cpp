d7081dac5f345d6884f67ea2551a955f0d64cf1f
Jessica <jiaohongyan>
2019-04-25 21:55:44 +0800

    fix

---
+++ Jessica/58.Plus One.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size() - 1; i >= 0; i--)   
+        {
+            if(digits[i] != 9)
+            {
+                digits[i] ++;
+                break;
+            }
+            digits[i] = 0;
+        }
+        if(digits[0] == 0)
+            digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
+
