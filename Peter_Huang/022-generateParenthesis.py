a3e39055c6664f7570841b526c03f08c2795eba2
Peter Huang <huangchao0623@126.com>
2019-09-28 14:53:46 +0800

    new

--- Peter_Huang/022-generateParenthesis.py
+++ Peter_Huang/022-generateParenthesis.py
@@ -1,24 +1,41 @@
+import itertools
 class Solution:
     def generateParenthesis(self, n):
-        res = [];
-        str = ''
-        for i in range(0,n):
-            str += '('
-        for i in range(0,n):
-            str += ')'
-        print(str)
-        res.append(str)
-        for i in range(n - 1,0,-1):
-            for j in range(n,2 * n - 1):
-                temp_str = list(str)
-                a = temp_str[i]
-                temp_str[i] = temp_str[j]
-                temp_str[j] = a
-                b = ''.join(temp_str)
-                res.append(b)
-        return res
+        if n == 0:
+            return ['']
+        elif n == 1:
+            return ['()']
+        resStr = ''
+        resList = []
+        d = []
+        for i in range(0, n):
+            d.append(i)
+        # for i in range(0, n):
+        d = itertools.product(d, repeat=n)
+        print('d:', d)
+        for item in d:
+            print(item)
+            for i in item:
+                resStr = self.insertParenthesis(i, resStr)
+            if resStr in resList:
+                resStr = ''
+                continue
+            else:
+                resList.append(resStr)
+                resStr = ''
+        return resList   
 
+    def insertParenthesis(self, index, parenthesisStr):
+        if index == 0:
+            return parenthesisStr + '()'
+        elif index == 1:
+            return '(' + parenthesisStr + ')'
+        else:
+            return '()' + parenthesisStr
 if __name__ == "__main__":
     s = Solution()
-    res = s.generateParenthesis(3);
+    res = s.generateParenthesis(4)
+    for item in ["(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())(())","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()"]:
+        if item not in res:
+            print(item)
     print(res)
